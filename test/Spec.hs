7ad73f32deb0c588f86734e7e0fb487b85d9fd30
Kárándi Tamás <tkarandi@gmail.com>
2020-11-22 21:36:06 +0100

    Remove unnecessary descriptions

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,7 +1,6 @@
 import Test.HUnit
 import AoC15.Day01
 
---main :: IO ()
 main = runTestTT tests
 
 tests = TestList
@@ -10,20 +9,20 @@ tests = TestList
             ]
 
 testAoC201501a = TestList
-            [ "AoC 2015/01a" ~:  0 ~=? (aoc201501a "(())")
-            , "AoC 2015/01a" ~:  0 ~=? (aoc201501a "()()")
-            , "AoC 2015/01a" ~:  3 ~=? (aoc201501a "(()(()(")
-            , "AoC 2015/01a" ~:  3 ~=? (aoc201501a "))(((((")
-            , "AoC 2015/01a" ~:(-1)~=? (aoc201501a "))(")
-            , "AoC 2015/01a" ~:(-3)~=? (aoc201501a ")())())")
+            [ 0 ~=? (aoc201501a "(())")
+            , 0 ~=? (aoc201501a "()()")
+            , 3 ~=? (aoc201501a "(()(()(")
+            , 3 ~=? (aoc201501a "))(((((")
+            , (-1)~=? (aoc201501a "))(")
+            , (-3)~=? (aoc201501a ")())())")
             , TestCase $ do
                 input <- readFile "/home/tomi/code/github.com/karandit/adventofcode/test/input201501.txt"
                 assertEqual "AoC 2015/01a" 138 (aoc201501a input)
             ]
 
 testAoC201501b = TestList
-            [ "AoC 2015/01b" ~: 1 ~=? (aoc201501b ")")
-            , "AoC 2015/01b" ~: 5 ~=? (aoc201501b "()())")
+            [ 1 ~=? (aoc201501b ")")
+            , 5 ~=? (aoc201501b "()())")
             , TestCase $ do
                 input <- readFile "/home/tomi/code/github.com/karandit/adventofcode/test/input201501.txt"
                 assertEqual "AoC 2015/01b" 1771 (aoc201501b input)
