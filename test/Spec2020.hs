9dbe6ce93febfe7529aa513e60a3fc71d4532058
Kárándi Tamás <tkarandi@gmail.com>
2020-12-03 20:09:45 +0100

    Refactor the structure of Spec2020

--- test/Spec2020.hs
+++ test/Spec2020.hs
@@ -8,31 +8,23 @@ import AoC2020.Day02
 import AoC2020.Day03
 
 tests = TestList
-            [ testAoC202001a
-            , testAoC202001b
-            , testAoC202002a
-            , testAoC202002b
-            , testAoC202003a
-            , testAoC202003b
-            ]
-
-testAoC202001a = TestList
+          -- Day01
+          [ TestList
             [ 514579 ~=? (aoc202001a [1721,979,366,299, 675, 1456])
             , TestCase $ do
                 input <- readFile "test/input202001.txt"
                 let nums = map (read :: String -> Int) $ lines input
                 assertEqual "AoC 2020/01a" 1007104 (aoc202001a nums)
             ]
-
-testAoC202001b = TestList
+          , TestList
             [ 241861950 ~=? (aoc202001b [1721,979,366,299, 675, 1456])
             , TestCase $ do
                 input <- readFile "test/input202001.txt"
                 let nums = map (read :: String -> Int) $ lines input
                 assertEqual "AoC 2020/01b" 18847752 (aoc202001b nums)
             ]
-
-testAoC202002a = TestList
+          -- Day02
+          , TestList
             [ 1 ~=? (aoc202002a [((1,3), 'a', "abcde")])
             , 0 ~=? (aoc202002a [((1,3), 'b', "cdefg")])
             , 1 ~=? (aoc202002a [((2,9), 'c', "ccccccccc")])
@@ -40,8 +32,7 @@ testAoC202002a = TestList
                 input <- readFile "test/input202002.txt"
                 assertEqual "AoC 2020/02a" 603 (aoc202002a $ parseInput202002 input)
             ]
-
-testAoC202002b = TestList
+          , TestList
             [ 1 ~=? (aoc202002b [((1,3), 'a', "abcde")])
             , 0 ~=? (aoc202002b [((1,3), 'b', "cdefg")])
             , 0 ~=? (aoc202002b [((2,9), 'c', "ccccccccc")])
@@ -49,8 +40,8 @@ testAoC202002b = TestList
                 input <- readFile "test/input202002.txt"
                 assertEqual "AoC 2020/02b" 404 (aoc202002b $ parseInput202002 input)
             ]
-
-testAoC202003a = TestList
+          -- Day03
+          , TestList
             [ TestCase $ do
                 input <- readFile "test/input202003_sample.txt"
                 assertEqual "AoC 2020/03a" 7 (aoc202003a input)
@@ -58,8 +49,7 @@ testAoC202003a = TestList
                 input <- readFile "test/input202003.txt"
                 assertEqual "AoC 2020/03a" 191 (aoc202003a input)
             ]
-
-testAoC202003b = TestList
+          , TestList
             [ TestCase $ do
                 input <- readFile "test/input202003_sample.txt"
                 assertEqual "AoC 2020/03b" 336 (aoc202003b input)
@@ -67,4 +57,5 @@ testAoC202003b = TestList
                 input <- readFile "test/input202003.txt"
                 assertEqual "AoC 2020/03b" 1478615040 (aoc202003b input)
             ]
+          ]
 
