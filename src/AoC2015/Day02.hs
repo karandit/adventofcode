fc3c04f6a7f9b3502c740a3a0b56e8ac8146f375
Kárándi Tamás <tkarandi@gmail.com>
2020-12-06 13:03:50 +0100

    Use utils in 2015/02

--- src/AoC2015/Day02.hs
+++ src/AoC2015/Day02.hs
@@ -4,21 +4,16 @@ module AoC2015.Day02
 , parseInput201502)
 where
 
-aoc201502a [a,b,c] = 
-    let surfaces = [a*b, a*c, b*c]    
-    in 2 * (sum surfaces) + minimum surfaces
+import Utils
+
+parseInput201502 =
+  let readNums = map readInt . words . replace 'x' ' '
+  in  map readNums . lines
+
+aoc201502a [a,b,c] =
+  let surfaces = [a*b, a*c, b*c]
+  in 2 * (sum surfaces) + minimum surfaces
 
 aoc201502b ns@[a,b,c] =
-    let perimeters = [a+b, a+c, b+c]    
-    in 2 * (minimum perimeters) + product ns
-    
-parseInput201502 input =
-    let fileLines = lines input
-
-        readInt :: String -> Int
-        readInt = read
-
-        readNums line = map (readInt) $ words $ map (\c -> if c == 'x' then ' ' else c) line
-    in  map readNums fileLines
-
-
+  let perimeters = [a+b, a+c, b+c]
+  in 2 * (minimum perimeters) + product ns
