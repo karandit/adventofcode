1ca45653ab79e70bd53484da594f1fe827dcad27
Kárándi Tamás <tkarandi@gmail.com>
2020-12-07 23:43:53 +0100

    Simplify 2020/06 by using foldl1

--- src/AoC2020/Day06.hs
+++ src/AoC2020/Day06.hs
@@ -3,19 +3,10 @@ module AoC2020.Day06
 , aoc202006b
 ) where
 
-import Data.List
-import qualified Data.Map as M
-import Utils
+import Data.List (union, intersect)
+import Data.List.Utils (split)
 
-fold1 (store,count) ""  = (M.empty, count + M.size store)
-fold1 (store,count) row = (M.union store $ freq row, count) 
+foldQuestionsBy f = sum . map (length . foldl1 f) . split [""] . lines
 
-fold2 (store,count) ""  = ([], count + (length $ foldl (intersect) (head store) (tail store)))
-fold2 (store,count) row = (row:store , count) 
-
-foldBlocks folder initStore input = 
-  let res = foldl folder (initStore, 0) $ lines input 
-  in snd $ folder res ""
-
-aoc202006a = foldBlocks fold1 M.empty
-aoc202006b = foldBlocks fold2 []
+aoc202006a = foldQuestionsBy union
+aoc202006b = foldQuestionsBy intersect
