05530bb9be879ba3afd82b64cb4edf20ee70e6ee
Kárándi Tamás <tkarandi@gmail.com>
2022-02-06 00:32:53 +0100

    Solve 2021/02

---
+++ src/AoC2021/Day02.hs
@@ -0,0 +1,19 @@
+module AoC2021.Day02
+( aoc202102
+) where
+
+import Utils (readInt, (|>))
+
+parseMove ('f':'o':'r':'w':'a':'r':'d':' ':x) = (readInt x, 0)
+parseMove ('d':'o':'w':'n':x)                 = (0, readInt x)
+parseMove ('u':'p':x)                         = (0, -(readInt x))
+
+aoc202102 input = (part1, part2) where
+    moves = input |> lines |> map parseMove
+
+    part1 = moves
+            |> foldl1 (\(x1, y1) (x2,y2) -> (x1+x2, y1+y2))
+            |> \(x, y) -> x * y
+    part2 = moves
+            |> foldl (\(x1, y1, aim) (x2,y2) -> (x1+x2, y1+(x2*(aim+y2)), aim+y2)) (0,0,0)
+            |> \(x, y, _) -> x * y
--- test/Spec2021.hs
+++ test/Spec2021.hs
@@ -4,15 +4,19 @@ module Spec2021
 
 import Test.HUnit
 import AoC2021.Day01
+import AoC2021.Day02
+
+assertAoC day aocFunc sampleAns myAns =
+             TestList
+            [ TestCase $ do
+                input <- readFile ("inputs/2021/day" ++ day ++ "_sample.txt")
+                assertEqual ("AoC 2021/" ++ day ++ " sample") sampleAns (aocFunc input)
+            , TestCase $ do
+                input <- readFile ("inputs/2021/day" ++ day ++ ".txt")
+                assertEqual ("AoC 2021/" ++ day) myAns (aocFunc input)
+            ]
 
 tests = TestList
-          -- Day01
-          [ TestList
-            [ TestCase $ do
-                input <- readFile "inputs/2021/day01_sample.txt"
-                assertEqual "AoC 2021/01 sample" (7, 5) (aoc202101 input)
-            , TestCase $ do
-                input <- readFile "inputs/2021/day01.txt"
-                assertEqual "AoC 2021/01" (1532, 1571) (aoc202101 input)
-            ]
+          [ assertAoC "01" aoc202101 (7, 5) (1532, 1571)
+          , assertAoC "02" aoc202102 (150, 900) (1635930, 1781819478)
           ]
