9981af14a1b8c97e143602c96f55f6b44705ebf6
Kárándi Tamás <tkarandi@gmail.com>
2022-02-07 00:03:12 +0100

    Solve 2021/13

---
+++ src/AoC2021/Day13.hs
@@ -0,0 +1,45 @@
+module AoC2021.Day13
+( aoc202113
+) where
+
+import Data.List
+import Data.List.Utils (split)
+import Data.Set (Set)
+import qualified Data.Set as Set
+
+import Utils (readInt, (|>))
+
+type Dot = (Int, Int)
+type Dots = Set Dot
+type Instr = (Char, Int)
+
+aoc202113 :: String -> (Int, [String])
+aoc202113 input = (part1, part2) where
+
+    [xyLines, instrLines]= input |> lines |> split [""]
+
+    inputDots :: Dots
+    inputDots = xyLines
+                |> map (\xyLine -> xyLine |> split "," |> map readInt |> \[xI,yI]->(xI, yI))
+                |> Set.fromList
+
+    instrs :: [Instr]
+    instrs = instrLines 
+                |> map (\('f':'o':'l':'d':' ':'a':'l':'o':'n':'g':' ':axis:'=':nr) -> (axis, readInt nr))
+
+    foldDot :: Instr -> Dot -> Dot
+    foldDot ('x', lx) (x, y) = (lx - (abs (x - lx)), y)
+    foldDot ('y', ly) (x, y) = (x, ly - (abs (y - ly)))
+
+    foldDots :: Dots -> Instr -> Dots
+    foldDots dots instr = Set.map (foldDot instr) dots
+
+    part1 :: Int
+    part1 = instrs |> head |> foldDots inputDots |> length
+    dots2 :: Dots
+    dots2 = instrs |> foldl' foldDots inputDots
+
+    maxX = dots2 |> Set.map fst |> Set.findMax
+    maxY = dots2 |> Set.map snd |> Set.findMax
+    part2 :: [String]
+    part2 = [ [if Set.member (x, y) dots2 then 'x' else ' ' | x <- [0..maxX]] | y <- [0..maxY]]
--- test/Spec2021.hs
+++ test/Spec2021.hs
@@ -9,22 +9,38 @@ import AoC2021.Day03
 import AoC2021.Day04
 import AoC2021.Day05
 import AoC2021.Day06
+import AoC2021.Day13
 
-assertAoC day aocFunc sampleAns myAns =
-             TestList
-            [ TestCase $ do
-                input <- readFile ("inputs/2021/day" ++ day ++ "_sample.txt")
-                assertEqual ("AoC 2021/" ++ day ++ " sample") sampleAns (aocFunc input)
-            , TestCase $ do
-                input <- readFile ("inputs/2021/day" ++ day ++ ".txt")
-                assertEqual ("AoC 2021/" ++ day) myAns (aocFunc input)
-            ]
+assertAoC day aocFunc sampleAns myAns = TestList
+    [ TestCase $ do
+        input <- readFile ("inputs/2021/day" ++ day ++ "_sample.txt")
+        assertEqual ("AoC 2021/" ++ day ++ " sample") sampleAns (aocFunc input)
+    , TestCase $ do
+        input <- readFile ("inputs/2021/day" ++ day ++ ".txt")
+        assertEqual ("AoC 2021/" ++ day) myAns (aocFunc input)
+    ]
+
+day13BigO = [
+    "xxxxx",
+    "x   x",
+    "x   x",
+    "x   x",
+    "xxxxx"]
+
+day13BLHFJPJF = [
+    "xxx  x    x  x xxxx   xx xxx    xx xxxx",
+    "x  x x    x  x x       x x  x    x x   ",
+    "xxx  x    xxxx xxx     x x  x    x xxx ",
+    "x  x x    x  x x       x xxx     x x   ",
+    "x  x x    x  x x    x  x x    x  x x   ",
+    "xxx  xxxx x  x x     xx  x     xx  x   "]
 
 tests = TestList
-          [ assertAoC "01" aoc202101 (7, 5) (1532, 1571)
-          , assertAoC "02" aoc202102 (150, 900) (1635930, 1781819478)
-          , assertAoC "03" aoc202103 (198, 230) (3633500, 4550283)
-          , assertAoC "04" aoc202104 (4512, 1924) (33462, 30070)
-          , assertAoC "05" aoc202105 (5, 12) (4745, 18442)
-          , assertAoC "06" aoc202106 (5934, 26984457539) (356190, 1617359101538)
-          ]
+    [ assertAoC "01" aoc202101 (7, 5) (1532, 1571)
+    , assertAoC "02" aoc202102 (150, 900) (1635930, 1781819478)
+    , assertAoC "03" aoc202103 (198, 230) (3633500, 4550283)
+    , assertAoC "04" aoc202104 (4512, 1924) (33462, 30070)
+    , assertAoC "05" aoc202105 (5, 12) (4745, 18442)
+    , assertAoC "06" aoc202106 (5934, 26984457539) (356190, 1617359101538)
+    , assertAoC "13" aoc202113 (17, day13BigO) (712, day13BLHFJPJF)
+    ]
