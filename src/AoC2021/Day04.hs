848957bc5ea2d2505b9381f4717a48ad04cbbfbd
Kárándi Tamás <tkarandi@gmail.com>
2022-02-06 01:22:37 +0100

    Solve 2021/04

---
+++ src/AoC2021/Day04.hs
@@ -0,0 +1,63 @@
+module AoC2021.Day04
+( aoc202104
+) where
+
+import Data.List
+import qualified Data.Map as M
+import qualified Data.List.Utils as LL
+import qualified Data.String.Utils as SS
+
+import Utils (readInt, (|>))
+
+type Coord = (Int, Int)
+type Board = M.Map Coord (Int, Bool)
+
+parseBoard :: [String] -> Board
+parseBoard rows = rows
+                |> zip [0..]
+                |> map (\(rowIdx,row) ->
+                    row
+                    |> SS.lstrip |> SS.replace "  " " "
+                    |> LL.split " "
+                    |> zip [0..]
+                    |> map (\(colIdx,c) -> ((rowIdx, colIdx), (readInt c, False))))
+                |> concat |> M.fromList
+
+
+findBingo :: [Board] -> Int -> [Board] -> [Int] -> ([Board], [Int])
+findBingo [] nr newBoards newBingos = (newBoards, newBingos)
+findBingo (board:boards) nr newBoards newBingos =
+       let
+           foundCoord = board |> M.toList |> find (\(_, (v, _)) -> nr == v)
+        in case foundCoord of
+            Nothing -> findBingo boards nr (board:newBoards) newBingos
+            Just ((row,col),_) -> let board' = M.adjust (\(v, _) -> (v, True)) (row, col) board
+                                      isBingo = checkBingo (row, col) board'
+                                  in if isBingo then findBingo boards nr newBoards ((bingoScore board' nr):newBingos)
+                                                else findBingo boards nr (board':newBoards) newBingos
+
+checkBingo :: Coord -> Board -> Bool
+checkBingo (row, col) board =
+    let isFiveMarked coords = coords |> all (\coord -> M.lookup coord board |> maybe False snd)
+    in    isFiveMarked (zip (cycle [row]) [0..4])
+       || isFiveMarked (zip [0..4] (cycle [col]))
+
+bingoScore :: Board -> Int -> Int
+bingoScore board nr =
+    let unmarkedsSum = board |> M.toList |> filter (\(_, (_, marked)) -> not marked) |> map (\(_, (v, _)) -> v) |> sum
+    in nr * unmarkedsSum
+
+playBingoAll [] _ bingos = bingos
+playBingoAll _ [] bingos = bingos
+playBingoAll boards (nr:nrs) bingos =
+        let (newBoards, newBingos) = findBingo boards nr [] []
+        in playBingoAll newBoards nrs (newBingos ++ bingos)
+
+aoc202104 input = (part1, part2) where
+    inputs = input |> lines |> LL.split [""]
+    nrs = inputs |> head |> head |> LL.split "," |> map readInt
+    boards = inputs |> tail |> map parseBoard
+    bingos = playBingoAll boards nrs []
+
+    part1 = last bingos
+    part2 = head bingos
--- test/Spec2021.hs
+++ test/Spec2021.hs
@@ -6,6 +6,7 @@ import Test.HUnit
 import AoC2021.Day01
 import AoC2021.Day02
 import AoC2021.Day03
+import AoC2021.Day04
 
 assertAoC day aocFunc sampleAns myAns =
              TestList
@@ -21,4 +22,5 @@ tests = TestList
           [ assertAoC "01" aoc202101 (7, 5) (1532, 1571)
           , assertAoC "02" aoc202102 (150, 900) (1635930, 1781819478)
           , assertAoC "03" aoc202103 (198, 230) (3633500, 4550283)
+          , assertAoC "04" aoc202104 (4512, 1924) (33462, 30070)
           ]
