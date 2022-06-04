f95c1100ecbe88f4fa6940af28d75541cf9fc4d5
Kárándi Tamás <tkarandi@gmail.com>
2022-06-04 18:20:38 +0200

    Fix haskell paths in ci after refactoring

--- haskell/test/unittests/AoCAsserts.hs
+++ haskell/test/unittests/AoCAsserts.hs
@@ -9,5 +9,5 @@ import Test.Tasty.HUnit
 assertAoC :: (Eq a, Show a) => String -> String -> (String -> a) -> a -> TestTree
 assertAoC year day aocFunc answer = testCase ("Day " ++ day) $ do
   let path = year ++ "/day" ++ day
-  input <- readFile ("inputs/" ++ path ++ ".txt")
+  input <- readFile ("../inputs/" ++ path ++ ".txt")
   assertEqual ("AoC " ++ path) answer (aocFunc input)
