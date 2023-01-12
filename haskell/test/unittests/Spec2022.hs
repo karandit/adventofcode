e24deebe6114d0026d17310d098c97aa873d894a
Kárándi Tamás <tkarandi@gmail.com>
2023-01-12 10:54:57 +0100

    Shorten tests

--- haskell/test/unittests/Spec2022.hs
+++ haskell/test/unittests/Spec2022.hs
@@ -20,8 +20,11 @@ import AoCAsserts (assertAoC)
 import Test.Tasty
 import Test.Tasty.HUnit
 
-assertAoC2022 :: (Eq a, Show a) => String -> (String -> a) -> a -> TestTree
-assertAoC2022 = assertAoC "2022"
+assert2022 :: (Eq a, Show a) => (String -> a) -> [(String, a)] -> TestTree
+assert2022 sut inputs =
+    let day = take 2 $ fst $ head $ inputs
+    in testGroup ("Day " ++ day) $
+        map (\(fileName, expected) -> assertAoC "2022" fileName sut expected) inputs
 
 day10Sample =
   [ "##..##..##..##..##..##..##..##..##..##..",
@@ -41,34 +44,21 @@ day10BPJAZGAP =
     "###..#.....##..#..#.####..###.#..#.#...."
   ]
 
+tests :: TestTree
 tests =
   testGroup
     "2022"
-    [ assertAoC2022 "01_sample" aoc202201 (24000, 45000),
-      assertAoC2022 "01" aoc202201 (72240, 210957),
-      assertAoC2022 "02_sample" aoc202202 (15, 12),
-      assertAoC2022 "02" aoc202202 (11841, 13022),
-      assertAoC2022 "03_sample" aoc202203 (157, 70),
-      assertAoC2022 "03" aoc202203 (7824, 2798),
-      assertAoC2022 "04_sample" aoc202204 (2, 4),
-      assertAoC2022 "04" aoc202204 (496, 847),
-      assertAoC2022 "05_sample" aoc202205 ("CMZ", "MCD"),
-      assertAoC2022 "05" aoc202205 ("RFFFWBPNS", "CQQBBJFCS"),
-      assertAoC2022 "06_sample" aoc202206 ([7, 5, 6, 10, 11], [19, 23, 23, 29, 26]),
-      assertAoC2022 "06" aoc202206 ([1100], [2421]),
-      assertAoC2022 "07_sample" aoc202207 (95437, 24933642),
-      assertAoC2022 "07" aoc202207 (1770595, 2195372),
-      assertAoC2022 "08_sample" aoc202208 (21, 8),
-      assertAoC2022 "08" aoc202208 (1719, 590824),
-      assertAoC2022 "09_sample" aoc202209 (13, 1),
-      assertAoC2022 "09_large" aoc202209 (88, 36),
-      assertAoC2022 "09" aoc202209 (6498, 2531),
-      assertAoC2022 "10_sample" aoc202210 (13140, day10Sample),
-      assertAoC2022 "10" aoc202210 (15140, day10BPJAZGAP),
-      assertAoC2022 "11_sample" aoc202211 (10605, 2713310158),
-      assertAoC2022 "11" aoc202211 (112221, 25272176808),
-      assertAoC2022 "12_sample" aoc202212 (31, 29),
-      assertAoC2022 "12" aoc202212 (425, 418),
-      assertAoC2022 "13_sample" aoc202213 (13, 140),
-      assertAoC2022 "13" aoc202213 (6086, 27930)
+    [ assert2022 aoc202201 [("01S", (24000, 45000)), ("01", (72240, 210957))],
+      assert2022 aoc202202 [("02S", (15, 12)), ("02", (11841, 13022))],
+      assert2022 aoc202203 [("03S", (157, 70)), ("03", (7824, 2798))],
+      assert2022 aoc202204 [("04S", (2, 4)), ("04", (496, 847))],
+      assert2022 aoc202205 [("05S", ("CMZ", "MCD")), ("05", ("RFFFWBPNS", "CQQBBJFCS"))],
+      assert2022 aoc202206 [("06S", ([7, 5, 6, 10, 11], [19, 23, 23, 29, 26])), ("06", ([1100], [2421]))],
+      assert2022 aoc202207 [("07S", (95437, 24933642)), ("07", (1770595, 2195372))],
+      assert2022 aoc202208 [("08S", (21, 8)), ("08", (1719, 590824))],
+      assert2022 aoc202209 [("09S", (13, 1)), ("09L", (88, 36)), ("09", (6498, 2531))],
+      assert2022 aoc202210 [("10S", (13140, day10Sample)), ("10", (15140, day10BPJAZGAP))],
+      assert2022 aoc202211 [("11S", (10605, 2713310158)), ("11", (112221, 25272176808))],
+      assert2022 aoc202212 [("12S", (31, 29)), ("12", (425, 418))],
+      assert2022 aoc202213 [("13S", (13, 140)), ("13", (6086, 27930))]
     ]
