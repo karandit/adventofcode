8596352ee223c59537a9676287bec46da7b6b044
Kárándi Tamás <tkarandi@gmail.com>
2023-01-12 12:44:31 +0100

    Shorten tests for 2021

--- haskell/test/unittests/Spec2021.hs
+++ haskell/test/unittests/Spec2021.hs
@@ -26,10 +26,13 @@ import AoCAsserts (assertAoC)
 import Test.Tasty
 import Test.Tasty.HUnit
 
-assertAoC2021 :: (Eq a, Show a) => String -> (String -> a) -> a -> TestTree
-assertAoC2021 = assertAoC "2021"
+assert2021 :: (Eq a, Show a) => (String -> a) -> [(String, a)] -> TestTree
+assert2021 sut inputs =
+    let day = take 2 $ fst $ head $ inputs
+    in testGroup ("Day " ++ day) $
+        map (\(fileName, expected) -> assertAoC "2021" fileName sut expected) inputs
 
-day13BigO =
+exp202113S =
   [ "xxxxx",
     "x   x",
     "x   x",
@@ -37,7 +40,7 @@ day13BigO =
     "xxxxx"
   ]
 
-day13BLHFJPJF =
+exp202113 =
   [ "xxx  x    x  x xxxx   xx xxx    xx xxxx",
     "x  x x    x  x x       x x  x    x x   ",
     "xxx  x    xxxx xxx     x x  x    x xxx ",
@@ -48,45 +51,24 @@ day13BLHFJPJF =
 
 tests =
   testGroup
-    "2021"
-    [ assertAoC2021 "01_sample" aoc202101 (7, 5),
-      assertAoC2021 "01" aoc202101 (1532, 1571),
-      assertAoC2021 "02_sample" aoc202102 (150, 900),
-      assertAoC2021 "02" aoc202102 (1635930, 1781819478),
-      assertAoC2021 "03_sample" aoc202103 (198, 230),
-      assertAoC2021 "03" aoc202103 (3633500, 4550283),
-      assertAoC2021 "04_sample" aoc202104 (4512, 1924),
-      assertAoC2021 "04" aoc202104 (33462, 30070),
-      assertAoC2021 "05_sample" aoc202105 (5, 12),
-      assertAoC2021 "05" aoc202105 (4745, 18442),
-      assertAoC2021 "06_sample" aoc202106 (5934, 26984457539),
-      assertAoC2021 "06" aoc202106 (356190, 1617359101538),
-      assertAoC2021 "07_sample" aoc202107 (37, 168),
-      assertAoC2021 "07" aoc202107 (344735, 96798233),
-      assertAoC2021 "08_sample" aoc202108 (26, 61229),
-      assertAoC2021 "08" aoc202108 (554, 990964),
-      assertAoC2021 "09_sample" aoc202109 (15, 1134),
-      assertAoC2021 "09" aoc202109 (439, 900900),
-      assertAoC2021 "10_sample" aoc202110 (26397, 288957),
-      assertAoC2021 "10" aoc202110 (411471, 3122628974),
-      assertAoC2021 "11_sample" aoc202111 (1656, 195),
-      assertAoC2021 "11" aoc202111 (1647, 348),
-      assertAoC2021 "12_small" aoc202112 (10, 36),
-      assertAoC2021 "12_large1" aoc202112 (19, 103),
-      assertAoC2021 "12_large2" aoc202112 (226, 3509),
-      assertAoC2021 "12" aoc202112 (4241, 122134),
-      assertAoC2021 "13_sample" aoc202113 (17, day13BigO),
-      assertAoC2021 "13" aoc202113 (712, day13BLHFJPJF),
-      assertAoC2021 "14_sample" aoc202114 (1588, 2188189693529),
-      assertAoC2021 "14" aoc202114 (2549, 2516901104210),
-      assertAoC2021 "15_sample" aoc202115 (40, 315),
-      assertAoC2021 "15" aoc202115 (739, 3040),
-      assertAoC2021 "16_sample" aoc202116 (31, 54),
-      assertAoC2021 "16" aoc202116 (993, 144595909277),
-      assertAoC2021 "17_sample" aoc202117 (45, 112),
-      assertAoC2021 "17" aoc202117 (12561, 3785),
-      assertAoC2021 "18_sample" aoc202118 (4140, 3993),
-      assertAoC2021 "18" aoc202118 (4173, 4706),
-      assertAoC2021 "25_sample" aoc202125 58,
-      assertAoC2021 "25" aoc202125 474
+    "Year 2021"
+    [ assert2021 aoc202101 [("01S", (7, 5)), ("01", (1532, 1571))],
+      assert2021 aoc202102 [("02S", (150, 900)), ("02", (1635930, 1781819478))],
+      assert2021 aoc202103 [("03S", (198, 230)), ("03", (3633500, 4550283))],
+      assert2021 aoc202104 [("04S", (4512, 1924)), ("04", (33462, 30070))],
+      assert2021 aoc202105 [("05S", (5, 12)), ("05", (4745, 18442))],
+      assert2021 aoc202106 [("06S", (5934, 26984457539)), ("06", (356190, 1617359101538))],
+      assert2021 aoc202107 [("07S", (37, 168)), ("07", (344735, 96798233))],
+      assert2021 aoc202108 [("08S", (26, 61229)), ("08", (554, 990964))],
+      assert2021 aoc202109 [("09S", (15, 1134)), ("09", (439, 900900))],
+      assert2021 aoc202110 [("10S", (26397, 288957)), ("10", (411471, 3122628974))],
+      assert2021 aoc202111 [("11S", (1656, 195)), ("11", (1647, 348))],
+      assert2021 aoc202112 [("12S", (10, 36)), ("12M", (19, 103)), ("12L", (226, 3509)), ("12", (4241, 122134))],
+      assert2021 aoc202113 [("13S", (17, exp202113S)), ("13", (712, exp202113))],
+      assert2021 aoc202114 [("14S", (1588, 2188189693529)), ("14", (2549, 2516901104210))],
+      assert2021 aoc202115 [("15S", (40, 315)), ("15", (739, 3040))],
+      assert2021 aoc202116 [("16S", (31, 54)), ("16", (993, 144595909277))],
+      assert2021 aoc202117 [("17S", (45, 112)), ("17", (12561, 3785))],
+      assert2021 aoc202118 [("18S", (4140, 3993)), ("18", (4173, 4706))],
+      assert2021 aoc202125 [("25S", 58), ("25", 474)]
     ]
