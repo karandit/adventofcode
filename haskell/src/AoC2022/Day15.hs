53f8f8551c49e522150fb5c6084d56261a0946bf
Kárándi Tamás <tkarandi@gmail.com>
2023-01-15 14:46:18 +0100

    Get rid of stripPrefix in 2022/15

--- haskell/src/AoC2022/Day15.hs
+++ haskell/src/AoC2022/Day15.hs
@@ -5,7 +5,7 @@ module AoC2022.Day15
 where
 
 import Utils (readInt, (|>))
-import Data.List (sortBy, find, stripPrefix)
+import Data.List (sortBy, find)
 import Data.List.Utils (split)
 import qualified Data.Maybe as Maybe
 import qualified Data.Map as M
@@ -14,11 +14,10 @@ import qualified Data.Range  as R
 manhattan (x1, y1) (x2, y2) = abs (x1 - x2) + abs (y1 - y2)
 
 aoc202215 inputY input = (part1, part2) where
-    parseSensor s = s
-      |> stripPrefix "Sensor at " |> Maybe.fromMaybe "" |> split ":"
-      |> (\[a,b] -> (a, b |> stripPrefix " closest beacon is at " |> Maybe.fromMaybe ""))
-      |> (\(a,b) -> (a |> split ", ", b |> split ", "))
-      |> (\(['x':'=':sx, 'y':'=':sy], ['x':'=':bx, 'y':'=':by]) -> ((readInt sx, readInt sy),(readInt bx,readInt by)))
+    parseSensor s = s |> split ":" |> map words
+        |> (\[["Sensor", "at", 'x':'=':sx, 'y':'=':sy],
+              ["closest", "beacon", "is", "at", 'x':'=':bx, 'y':'=':by]] ->
+            ((readInt $ init sx, readInt sy), (readInt $ init bx, readInt by)))
 
     diamonds = [(s, b, manhattan s b) | (s, b) <- map parseSensor $ lines $ input]
 
@@ -34,9 +33,9 @@ aoc202215 inputY input = (part1, part2) where
         in ranges'') []
       |> R.fromRanges |> length
 
-    (foundSensor@(sx,sy), _, foundRad) = diamonds |> filter (\(s1, _, rad1) -> 
-                               diamonds |> find (\(s2, _, rad2) -> s1 /= s2 && manhattan s1 s2 == 2 + rad1 + rad2) |> Maybe.isJust)
-                       |> sortBy (\ (s0,_, m1) (s2,_, m2) -> compare m1 m2) -- get the smallest radius diamond
+    (foundSensor@(sx,sy), _, foundRad) = diamonds
+                       |> filter (\(s1, _, m1) -> diamonds |> find (\(s2, _, m2) -> s1 /= s2 && manhattan s1 s2 == 2 + m1 + m2) |> Maybe.isJust)
+                       |> sortBy (\(s0,_, m1) (s2,_, m2) -> compare m1 m2) -- get the smallest radius diamond
                        |> head
 
     perimeter = foundRad + 1
@@ -44,11 +43,12 @@ aoc202215 inputY input = (part1, part2) where
 
     notIncluded p = otherSensors |> any (\(si, _ , mi)-> manhattan si p <= mi) |> not
 
-    part2 = [1..perimeter] |> map (\d -> [(sx+perimeter-d, sy+d),
-                                          (sx-d, sy+perimeter-d),
-                                          (sx - (perimeter-d), sy - d),
-                                          (sx + d, sy - (perimeter-d))])
-                   |> concat
-                   |> find notIncluded
-                   |> fmap(\(x, y) -> x * 4000000 + y)
-                   |> Maybe.fromMaybe (-1)
+    part2 = [1..perimeter]
+            |> map (\d -> [(sx+perimeter-d, sy+d),
+                           (sx-d, sy+perimeter-d),
+                           (sx - (perimeter-d), sy - d),
+                           (sx + d, sy - (perimeter-d))])
+            |> concat
+            |> find notIncluded
+            |> fmap(\(x, y) -> x * 4000000 + y)
+            |> Maybe.fromMaybe (-1)
