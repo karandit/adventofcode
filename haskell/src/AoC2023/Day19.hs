ca2584d87a76b6eb3342f80da06dbf815343c99c
Kárándi Tamás <tkarandi@gmail.com>
2024-02-18 22:41:13 +0100

    Solve 2023/19 in Haskell, part2

--- haskell/src/AoC2023/Day19.hs
+++ haskell/src/AoC2023/Day19.hs
@@ -6,11 +6,14 @@ import Utils (readInt, (|>))
 import Data.List.Utils (split, replace)
 import qualified Data.Map as M
 
-data Rule = Proc String | Cond (Char, Char, Int, String)  deriving (Show)
+data Rule = Proc String | Cond (Char, Char, Int, String) deriving (Show)
+type Part = M.Map Char Int -- for part 1
+type Ranges = M.Map Char (Int, Int) -- for part 2
 
 parsePart s = s |> replace "{" "" |> replace "}" "" |> split ","
     |> map (split "=")
-    |> map (\[k, v] -> (head k, readInt v)) |> M.fromList
+    |> map (\[k, v] -> (head k, readInt v))
+    |> M.fromList
 
 parseWorkflow s = s |> split "{"
     |> (\[name, r] -> (name, r |> replace "}" "" |> split "," |> map parseRule))
@@ -18,45 +21,52 @@ parseWorkflow s = s |> split "{"
 parseRule s = if elem ':' s
     then s |> split ":" |> \[r:c:val, dst] -> Cond (r, c, (readInt val), dst)
     else Proc s
-                
-main :: IO ()
-main = do 
-  input <- readFile "../inputs/2023/day19S.txt"
-  input <- readFile "../inputs/2023/day19.txt"
-  let
-  -- putStrLn $ "------------"
-  -- putStrLn $ unlines $ map show $ ws'
-  -- putStrLn $ ""
-  -- putStrLn $ unlines $ map show $ parts
-  -- putStrLn $ "------------"
-  -- putStrLn $ show $ procPart p1
-  -- putStrLn $ show $ part1
-  putStrLn $ show $ aoc202319 input
 
-aoc202319 input = (part1, "MISSING") where
+aoc202319 input = (part1, part2) where
   [ws, ps] = input |> lines |> split [""]
-  ws' = ws |> map parseWorkflow
-  wfs = ws' |> M.fromList
+  wfs = ws |> map parseWorkflow |> M.fromList
   parts = ps |> map parsePart
-  
-  p1 = head $ tail parts 
 
-  procRules part [Proc dst] = case dst of
-              "A" -> True
-              "R" -> False
-              _   -> procRules part (wfs M.! dst)
-  procRules part (Cond (fRat, fOp, val, dst):rs) = let
-        rat = (part M.! fRat) 
-        isMatching = case fOp of
-                  '<' -> rat < val
-                  '>' -> rat > val
+  --------------- PART 1 ---------------------------------------
+  procRules1 :: [Rule] -> Part -> Int
+  procRules1 [Proc dst] part = procDest1 dst part
+  procRules1 (Cond (ratingCat, op, val, dst):rules) part = let
+      rating = (part M.! ratingCat)
+      isMatching = case op of
+                '<' -> rating < val
+                '>' -> rating > val
       in if isMatching
-         then case dst of
-              "A" -> True
-              "R" -> False
-              _   -> procRules part (wfs M.! dst)
-         else procRules part rs
-      
-  procPart part = procRules part (wfs M.! "in")
-  accepteds = filter procPart parts
-  part1 = accepteds |> map (sum . M.elems) |> sum
+         then procDest1 dst part
+         else procRules1 rules part
+
+  procDest1 :: String -> Part -> Int
+  procDest1 dst part = case dst of
+      "A" -> part |> M.elems |> sum
+      "R" -> 0
+      _   -> procRules1 (wfs M.! dst) part
+
+  --------------- PART 2 ---------------------------------------
+  procRules2 :: [Rule] -> Ranges -> Int
+  procRules2 [rule@(Proc dst)] ranges = procDest2 dst ranges
+  procRules2 (rule@(Cond (ratingCat, op, val, dst)):rules) ranges = let
+      (lowerBound, upperBound) = (ranges M.! ratingCat)
+      (trueBounds, falseBounds) = case op of
+        '<' -> ( (lowerBound, min upperBound (val - 1))
+               , (max lowerBound val, upperBound))
+        '>' -> ((max lowerBound (val + 1), upperBound)
+               , (lowerBound, val))
+      trueRanges = M.insert ratingCat trueBounds ranges
+      trueBranch = procDest2 dst trueRanges
+
+      falseRanges = M.insert ratingCat falseBounds ranges
+      falseBranch = procRules2 rules falseRanges
+      in trueBranch + falseBranch
+
+  procDest2 :: String -> Ranges -> Int
+  procDest2 dst ranges = case dst of
+      "A" -> ranges |> M.elems |> map (\(lb, ub) -> ub - lb + 1) |> product
+      "R" -> 0
+      _   -> procRules2 (wfs M.! dst) ranges
+
+  part1 = parts |> map (procRules1 (wfs M.! "in")) |> sum
+  part2 = [(p, (1, 4000)) | p <- "xmas"] |> M.fromList |> procRules2 (wfs M.! "in")
