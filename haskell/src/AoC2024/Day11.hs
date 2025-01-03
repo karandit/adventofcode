b05f84b0fc66d136954d041fe9d2e114d187a646
Kárándi Tamás <tkarandi@gmail.com>
2025-01-03 20:52:32 +0100

    Use Int instead of String in 2024/11

--- haskell/src/AoC2024/Day11.hs
+++ haskell/src/AoC2024/Day11.hs
@@ -2,26 +2,22 @@ module AoC2024.Day11
 ( aoc202411
 ) where
 
-import Utils (readInt, (|>))
+import Utils (readInt)
 import Data.List (foldl')
 import qualified Data.Map as M
 
 aoc202411 input = (part1, part2) where
-  inputs = input |> lines |> head |> words
+  inputs = [readInt w | w <- words $ head $ lines $ input]
 
-  leftTrim [] = []
-  leftTrim [x] = [x]
-  leftTrim ('0':x) = leftTrim x
-  leftTrim n = n
-
-  change :: String -> [String]
+  change :: Int -> [Int]
+  change 0 = [1]
   change stone
-    | stone == "0"              = ["1"]
-    | length stone `mod` 2 == 0 = let n = length stone`div` 2 in [take n stone] ++ [leftTrim $ drop n stone]
-    | otherwise                 = [show $ readInt stone * 2024]
+    | (w, 0) <- length (show stone) `quotRem` 2
+    , (l, r) <- stone `quotRem` (10 ^ w) = [l, r]
+  change stone = [stone * 2024]
 
-  blink :: Int -> (M.Map (String, Int) Int, Int) -> String -> (M.Map (String, Int) Int, Int)
-  blink 0 (memo, acc) stone = (memo, acc + 1)
+  blink :: Int -> (M.Map (Int, Int) Int, Int) -> Int -> (M.Map (Int, Int) Int, Int)
+  blink 0 (memo, acc) _     = (memo, acc + 1)
   blink n (memo, acc) stone = case M.lookup (stone, n) memo of
     Nothing ->
       let (memo', v) = foldl' (blink (n-1)) (memo, 0) (change stone)
